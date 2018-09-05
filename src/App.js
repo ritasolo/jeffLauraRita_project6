de35b695df7e27f4e4d734cd26355fecc8cefe7c
Rita <contact@ritasolo.com>
2018-09-05 15:36:04 -0400

    Partials set up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <h1>This is an H1</h1>
+        <h2>This is an H2</h2>
+        <h3>This is an H3</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, nisi!
+          At quia reiciendis, ex saepe iure est sapiente fugiat distinctio odio
+          quibusdam quos molestiae excepturi aliquid incidunt iste nobis! Iure!
         </p>
       </div>
     );
