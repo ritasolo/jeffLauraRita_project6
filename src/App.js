2b85797d2c584977dc670e7fcafc584f8134fb6d
Rita <contact@ritasolo.com>
2018-09-06 20:55:03 -0400

    filtered through wines

--- src/App.js
+++ src/App.js
@@ -14,9 +14,20 @@ class App extends Component {
     super();
     this.state = {
       wineArray: [],
-      colour: "all",
+      colour: "red",
       price: "$",
-      cheapestWines: []
+      $: [],
+      $$: [],
+      $$$: [],
+      $$$$: [],
+      $white: [],
+      $red: [],
+      $$white: [],
+      $$red: [],
+      $$$white: [],
+      $$$red: [],
+      $$$$white: [],
+      $$$$red: []
     };
   }
 
@@ -45,7 +56,9 @@ class App extends Component {
   };
 
   componentDidMount() {
-    const wineRequests = [1, 2, 3, 4, 5, 6, 7].map(this.getWine);
+    const wineRequests = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(
+      this.getWine
+    );
     Promise.all(wineRequests).then(responses => {
       responses = responses
         .map(response => {
@@ -61,39 +74,149 @@ class App extends Component {
             item.package_unit_type === "bottle"
           );
         });
-      console.log(responses);
-      this.setState({
-        wineArray: responses
-      });
+      this.setState(
+        {
+          wineArray: responses
+        },
+        () => {
+          const fullArray = this.state.wineArray;
+          const $ = fullArray.filter(item => {
+            return item.price_in_cents > 600 && item.price_in_cents < 1000;
+          });
+          const $$ = fullArray.filter(item => {
+            return item.price_in_cents > 1000 && item.price_in_cents < 1400;
+          });
+          const $$$ = fullArray.filter(item => {
+            return item.price_in_cents > 1400 && item.price_in_cents < 1800;
+          });
+          const $$$$ = fullArray.filter(item => {
+            return item.price_in_cents > 1800 && item.price_in_cents < 2200;
+          });
+
+          const $white = $.filter(item => {
+            return item.secondary_category === "White Wine";
+          }).map(response => {
+            return {
+              id: response.id,
+              name: response.name,
+              price: `$${response.price_in_cents / 100}`,
+              imgURL: response.image_url,
+              onSale: response.has_clearance_sale
+            };
+          });
+
+          const $red = $.filter(item => {
+            return item.secondary_category === "Red Wine";
+          }).map(response => {
+            return {
+              id: response.id,
+              name: response.name,
+              price: `$${response.price_in_cents / 100}`,
+              imgURL: response.image_url,
+              onSale: response.has_clearance_sale
+            };
+          });
+
+          const $$red = $$.filter(item => {
+            return item.secondary_category === "Red Wine";
+          }).map(response => {
+            return {
+              id: response.id,
+              name: response.name,
+              price: `$${response.price_in_cents / 100}`,
+              imgURL: response.image_url,
+              onSale: response.has_clearance_sale
+            };
+          });
+
+          const $$white = $$.filter(item => {
+            return item.secondary_category === "Red Wine";
+          }).map(response => {
+            return {
+              id: response.id,
+              name: response.name,
+              price: `$${response.price_in_cents / 100}`,
+              imgURL: response.image_url,
+              onSale: response.has_clearance_sale
+            };
+          });
+
+          const $$$white = $$$.filter(item => {
+            return item.secondary_category === "White Wine";
+          }).map(response => {
+            return {
+              id: response.id,
+              name: response.name,
+              price: `$${response.price_in_cents / 100}`,
+              imgURL: response.image_url,
+              onSale: response.has_clearance_sale
+            };
+          });
+
+          const $$$red = $$$.filter(item => {
+            return item.secondary_category === "Red Wine";
+          }).map(response => {
+            return {
+              id: response.id,
+              name: response.name,
+              price: `$${response.price_in_cents / 100}`,
+              imgURL: response.image_url,
+              onSale: response.has_clearance_sale
+            };
+          });
+
+          const $$$$white = $$$$.filter(item => {
+            return item.secondary_category === "White Wine";
+          }).map(response => {
+            return {
+              id: response.id,
+              name: response.name,
+              price: `$${response.price_in_cents / 100}`,
+              imgURL: response.image_url,
+              onSale: response.has_clearance_sale
+            };
+          });
+
+          const $$$$red = $$$$.filter(item => {
+            return item.secondary_category === "Red Wine";
+          }).map(response => {
+            return {
+              id: response.id,
+              name: response.name,
+              price: `$${response.price_in_cents / 100}`,
+              imgURL: response.image_url,
+              onSale: response.has_clearance_sale
+            };
+          });
+
+          this.setState({
+            $,
+            $$,
+            $$$,
+            $$$$,
+            $white,
+            $red,
+            $$white,
+            $$red,
+            $$$white,
+            $$$red,
+            $$$$white,
+            $$$$red
+          });
+        }
+      );
     });
   }
 
-  // filterArray = array => {
-  //   array.filter(item => {
-  //     return (
-  //       item.price_in_cents < 2200 &&
-  //       item.package_unit_volume_in_milliliters === 750 &&
-  //       item.package_unit_type === "bottle"
-  //     );
-  //   });
-  // };
-
-  // filterByPrice = userPriceChoice => {
-  //   {
-  //     this.state.price === "$" ? this.state.wineArray.filter(item => {
-  //       return (
-  //         item.price_in_cents < 1000 )} : null;
-  //   }
-  // };
-
   render() {
-    // this.filterArray(this.wineArray);
-    // console.log(this.wineArray);
     return (
       <Router>
         <div className="App">
           <header>
             <h1>Plonk</h1>
+            {this.state.price === "$" && this.state.colour === "red"
+              ? console.log(this.state.$red)
+              : null}
           </header>
           <section>
             <Form />
