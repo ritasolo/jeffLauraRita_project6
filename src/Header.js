8ca3a1e2188204e379f074872a525f4eb2c93ae1
Jeff <j_lee92@hotmail.com>
2018-09-09 16:19:21 -0400

    Adding Firebase

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,64 @@
-import React,  { Component } from 'react'
+import React, { Component } from 'react';
+import firebase from './firebase';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import SavedList from './SavedList';
+
+const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
+
 
 class Header extends Component {
-
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+    }
+  }
+  login = () => {
+    auth.signInWithPopup(provider).then((res) => {
+      console.log(res)
+      this.setState({
+        user: res.user
+      })
+      this.props.appstate(this.state.user)
+    })
+  }
+  logout = () => {
+    auth.signOut().then(() => {
+      this.setState({
+        user: null
+      })
+    })
+  }
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          user
+        }, () => {
+          this.dbref = firebase.database().ref(this.state.user.uid)
+        })
+      }
+    });
+  }
 render(){
   return(
+    <Router>
     <div className="headerSection clearfix">
             <header>
               <h1>PLONK</h1>
+            {this.state.user
+              ?
+              <div>
+                <Link to="/">
+                  <button onClick={this.logout}>Logout</button>
+                </Link>
+                <Link to={`/user/${this.state.user.uid}`}>
+                  <a href="#">Favourites</a>
+                </Link>
+              </div>
+              : <button onClick={this.login}>Login</button>
+            }
             </header>
     <section className="hero">
     <div className="heroScreen">
@@ -18,6 +70,7 @@ render(){
     </div>
     </section>
     </div>
+    </Router>
   )
 }
 
