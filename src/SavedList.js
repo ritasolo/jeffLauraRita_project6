44fb96852e88a71a1078535e0f19aa6a55acccc6
Laura Kirkham <kirkham.laura@hotmail.com>
2018-09-11 12:43:58 -0400

    styled cellar

--- src/SavedList.js
+++ src/SavedList.js
@@ -6,38 +6,32 @@ class SavedList extends Component {
     return (
       <section className="savedList">
         <div className="wrapper clearfix">
-          <Link to="/">Back to Main Page</Link>
-          <h2>My Wines</h2>
+          <Link to="/"><p>Back to Main Page</p></Link>
+          <h2 className="myCellar">My Cellar</h2>
           {this.props.wineInfo.map(wine => {
-            return (
-              <div className="card wineList clearfix" key={wine.wineKey}>
+            return <div className="card wineList clearfix" key={wine.wineKey}>
                 <div className="cardWrapper clearfix">
                   <figure className="imageWrapper">
                     <img src={wine.wineImage} />
                   </figure>
                   <div className="cardSide">
                     <div className="wineTitle">
-                      <p>{wine.wineName}</p>
+                      <h3>{wine.wineName}</h3>
                     </div>
-                    <Link
-                      className="btn"
-                      to={`/products/${wine.wineId}`}
-                      target="_blank"
-                    >
-                      More Info
+                  <div className="savedListBtns">
+                    <Link to={`/products/${wine.wineId}`} target="_blank">
+                      <button className="btn smallBtn cellarBtn">
+                        See Details
+                      </button>
                     </Link>
 
-                    <button
-                      className="btn"
-                      onClick={() => this.props.deleteWine(wine.wineKey)}
-                      id={wine.wineKey}
-                    >
+                    <button className="btn smallBtn cellarBtn deleteBtn" onClick={() => this.props.deleteWine(wine.wineKey)} id={wine.wineKey}>
                       Delete Wine
                     </button>
                   </div>
+                  </div>
                 </div>
-              </div>
-            );
+              </div>;
           })}
         </div>
       </section>
