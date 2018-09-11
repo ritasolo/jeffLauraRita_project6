768920d4d30fe262b5d2e00c564ff96d7626203f
Rita <contact@ritasolo.com>
2018-09-11 17:37:29 -0400

    fixed precheck

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 // import Slider from "react-rangeslider";
 // import 'react-rangeslider/lib/index.css';
-import drinkingForest from "./assets/drinkingForest.jpg";
 
 class Form extends Component {
   constructor() {
@@ -16,11 +15,11 @@ class Form extends Component {
     // const { value } = this.state
     return (
       <section className="formContainer clearfix">
-      <div className="form clearfix">
-        <div className="formWrapper">
-          <form onSubmit={this.props.displayWines}>
-            <div className="filters clearfix">
-              {/* <input
+        <div className="form clearfix">
+          <div className="formWrapper">
+            <form onSubmit={this.props.displayWines}>
+              <div className="filters clearfix">
+                {/* <input
                 type="range"
                 min="1"
                 max="4"
@@ -39,7 +38,7 @@ class Form extends Component {
       onChange={this.handleChangePrice}
       step="1"/> */}
 
-              {/* <div className='slider'>
+                {/* <div className='slider'>
         <Slider
           min={1}
           max={4}
@@ -51,111 +50,116 @@ class Form extends Component {
         <div className='value'>{value}</div>
       </div> */}
 
-              <fieldset className="priceSelect">
-              <h3>Set your budget:</h3>
-                <ul>
-                  <li>
-                    <label className="radio inline" htmlFor="$">
-                    <input
-                      onClick={this.props.handleChangePrice}
-                      name="price"
-                      id="$"
-                      type="radio"
-                      value={this.state.price}
-                    />
-                     <span>&nbsp;$</span>
-                     </label>
-                  </li>
-                  <li>
-                    <label className="radio inline" htmlFor="$$"> 
-                    <input
-                      onClick={this.props.handleChangePrice}
-                      name="price"
-                      id="$$"
-                      type="radio"
-                      value={this.state.price}
-                    />
-                    <span> $$</span>
-                    </label>
-                  </li>
-                  <li>
-                    <label className="radio inline" htmlFor="$$$">
-                    <input
-                      onClick={this.props.handleChangePrice}
-                      name="price"
-                      id="$$$"
-                      type="radio"
-                      value={this.state.price}
-                    />
-                    <span> $$$</span>
-                    </label>
-                  </li>
-                  <li>
-                    <label className="radio inline" htmlFor="$$$$"> 
-                    <input
-                      onClick={this.props.handleChangePrice}
-                      name="price"
-                      id="$$$$"
-                      type="radio"
-                      value={this.state.price}
-                    />
-                    <span> $$$$</span>
-                    </label>
-                  </li>
-                </ul>
-              </fieldset>
+                <fieldset className="priceSelect">
+                  <h3>Set your budget:</h3>
+                  <ul>
+                    <li>
+                      <label className="radio inline" htmlFor="$">
+                        <input
+                          onClick={this.props.handleChangePrice}
+                          name="price"
+                          id="$"
+                          type="radio"
+                          value={this.state.price}
+                        />
+                        <span>&nbsp;$</span>
+                      </label>
+                    </li>
+                    <li>
+                      <label className="radio inline" htmlFor="$$">
+                        <input
+                          onClick={this.props.handleChangePrice}
+                          name="price"
+                          id="$$"
+                          type="radio"
+                          value={this.state.price}
+                        />
+                        <span>&nbsp;$$</span>
+                      </label>
+                    </li>
+                    <li>
+                      <label className="radio inline" htmlFor="$$$">
+                        <input
+                          onClick={this.props.handleChangePrice}
+                          name="price"
+                          id="$$$"
+                          type="radio"
+                          value={this.state.price}
+                        />
+                        <span>&nbsp;$$$</span>
+                      </label>
+                    </li>
+                    <li>
+                      <label className="radio inline" htmlFor="$$$$">
+                        <input
+                          onClick={this.props.handleChangePrice}
+                          name="price"
+                          id="$$$$"
+                          type="radio"
+                          value={this.state.price}
+                        />
+                        <span>&nbsp;$$$$</span>
+                      </label>
+                    </li>
+                  </ul>
+                </fieldset>
 
-              <fieldset className="colourSelect">
-                <h3>Pick your plonk:</h3>
-                <ul>
-                  <li>
-                    <label className="radio inline" htmlFor="red"> 
-                    <input
-                      onChange={this.props.handleChangeColour}
-                      name="colour"
-                      id="red"
-                      type="radio"
-                      value={this.state.colour}
-                    />
-                    <span>&nbsp;Red</span>
-                    </label>
-                  </li>
-                  <li>
-                    <label className="radio inline" htmlFor="white"> 
-                    <input
-                      onChange={this.props.handleChangeColour}
-                      name="colour"
-                      id="white"
-                      type="radio"
-                      value={this.state.colour}
-                    />
-                    <span> White</span>
-                    </label>
-                  </li>
-                  <li>
-                    <label className="radio inline" htmlFor="all">
-                    <input
-                      onChange={this.props.handleChangeColour}
-                      name="colour"
-                      id="all"
-                      type="radio"
-                      value={this.state.colour}
-                    />
-                    <span> All</span>
-                    </label>
-                  </li>
-                </ul>
-              </fieldset>
-            </div>
-          </form>
-          <button className="btn filterBtn" onClick={this.props.displayWines}>Bottoms Up</button>
-        </div>
-      </div>
-   <div className="formQuote">
-          <div className="formQuoteScreen">
-          <h2 className="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. </h2>
+                <fieldset className="colourSelect">
+                  <h3>Pick your plonk:</h3>
+                  <ul>
+                    <li>
+                      <label className="radio inline" htmlFor="red">
+                        <input
+                          onChange={this.props.handleChangeColour}
+                          name="colour"
+                          id="red"
+                          type="radio"
+                          value={this.state.colour}
+                        />
+                        <span>&nbsp;Red</span>
+                      </label>
+                    </li>
+                    <li>
+                      <label className="radio inline" htmlFor="white">
+                        <input
+                          onChange={this.props.handleChangeColour}
+                          name="colour"
+                          id="white"
+                          type="radio"
+                          value={this.state.colour}
+                        />
+                        <span>&nbsp;White</span>
+                      </label>
+                    </li>
+                    <li>
+                      <label className="radio inline" htmlFor="all">
+                        <input
+                          onChange={this.props.handleChangeColour}
+                          name="colour"
+                          id="all"
+                          type="radio"
+                          value={this.state.colour}
+                        />
+                        <span>&nbsp;All</span>
+                      </label>
+                    </li>
+                  </ul>
+                </fieldset>
+              </div>
+            </form>
+            <button className="btn filterBtn" onClick={this.props.displayWines}>
+              Bottoms Up
+            </button>
           </div>
-      </div>
+        </div>
+        <div className="formQuote">
+          <div className="formQuoteScreen">
+            <h2 className="quote">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor.{" "}
+            </h2>
+          </div>
+        </div>
       </section>
     );
   }
