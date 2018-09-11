9aedd8c535c8265d675d434191dbac2d5e0d8652
Rita <contact@ritasolo.com>
2018-09-11 17:48:36 -0400

    beautyyyy

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import SavedList from "./SavedList";
 import Nav from "./Nav";
-import { animateScroll as scroll } from 'react-scroll'
-
+import { animateScroll as scroll } from "react-scroll";
 
 class Header extends Component {
   constructor() {
@@ -12,12 +11,12 @@ class Header extends Component {
       user: null
     };
   }
-  handleSetActive = (to) => {
-  console.log(to);
-  }
+  handleSetActive = to => {
+    console.log(to);
+  };
   scrollTo = () => {
-  scroll.scrollTo(730);
-  }
+    scroll.scrollTo(730);
+  };
   render() {
     return (
       <div className="headerSection clearfix">
@@ -29,17 +28,12 @@ class Header extends Component {
                 <h2 className="heroHeading">
                   Lorem ipsum dolor sit amet, lorem noluisse.{" "}
                 </h2>
-<<<<<<< HEAD
-                <p className="heroCopy">
-                  Find your next bottle for under $22
-                </p>
-              {/* <Link activeClass="active" to=".formContainer" spy={true} smooth={true} offset={50} duration={500} onSetActive={this.handleSetActive}> */}
-                <button onClick={this.scrollTo} className="btn">Find Plonk</button>
-              {/* </Link> */}
-=======
                 <p className="heroCopy">Find your next bottle for under $22</p>
-                <button className="btn">Find Plonk</button>
->>>>>>> 0d4c87429ca79a06688aacfa70c5ad90ec46311b
+                {/* <Link activeClass="active" to=".formContainer" spy={true} smooth={true} offset={50} duration={500} onSetActive={this.handleSetActive}> */}
+                <button onClick={this.scrollTo} className="btn">
+                  Find Plonk
+                </button>
+                {/* </Link> */}
               </div>
             </div>
           </div>
